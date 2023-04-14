a71cf56616ca3cc1f80ad7026b0ee67c58e94b01
tpavaskar781 <tpavaskar781@gmail.com>
2023-04-14 15:26:53 +0530

    assignment4

---
+++ cypress/e2e/Assignment4.cy.js
@@ -0,0 +1,14 @@
+//// <reference types="Cypress" />
+//// <reference types="Cypress" />
+///reference types = 'cypress-xpath'
+
+describe("Assignment of dropdown handel",()=>{
+
+    it("Dropdown handel",()=>{
+        cy.visit("https://formstone.it/components/dropdown/demo/")
+        
+        
+    })
+
+
+})
