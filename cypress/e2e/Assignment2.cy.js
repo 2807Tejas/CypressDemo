3c1373ad9563cb7a66940b3a81be28e0f60137c3
tpavaskar781 <tpavaskar781@gmail.com>
2023-04-14 15:26:33 +0530

    assignment2

--- cypress/e2e/Assignment2.cy.js
+++ cypress/e2e/Assignment2.cy.js
@@ -3,15 +3,16 @@
 describe('Assignment2', () => {
     it("assignment",()=>{
 
-          cy.visit("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm")
+           cy.visit("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm")
            cy.get("[name='firstname']").type("Tejas",{force:true})
            cy.get("[name='lastname']").type("Pavaskar",{force:true})
            //cy.wait(5000)
-           cy.get("//tbody//tr[3]//input[1]").click()
-        //   cy.get("//tbody/tr[4]/td[2]/span[1]").click()
-        //   cy.get("//tbody/tr[5]").type("24/03/2023")
+           cy.get("[value='Male']").click()
+           cy.get("[value='2']").click({force:true})
+           cy.get("//table/tbody/tr[5]/td[2]/input").type("14/04/2023")
+          // cy.get("//tbody/tr[5]/td[2]/input").type("13/04/2023")
         //   cy.get("//tbody/tr[6]/td[2]/span[1]/input").click()
-        //   cy.get("/tbody/tr[6]/td[2]/span[2]/input").click()           //proffession
+           cy.get("[value='Manual Tester']").click()           //proffession
         //   cy.get("//tbody/tr[8]/td/span[2]/input[1] ").click()         // flovers of selenium 
           
 
