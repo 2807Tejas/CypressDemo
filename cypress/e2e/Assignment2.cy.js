36068b59294ffa570e8727a7be868d6dd4b70b72
tpavaskar781 <tpavaskar781@gmail.com>
2023-04-17 19:50:23 +0530

    pageobject

--- cypress/e2e/Assignment2.cy.js
+++ cypress/e2e/Assignment2.cy.js
@@ -7,12 +7,20 @@ describe('Assignment2', () => {
            cy.get("[name='firstname']").type("Tejas",{force:true})
            cy.get("[name='lastname']").type("Pavaskar",{force:true})
            //cy.wait(5000)
-           cy.get("[value='Male']").click()
+           cy.get("[value='Male']").click({force:true})
            cy.get("[value='2']").click({force:true})
-           cy.get("//table/tbody/tr[5]/td[2]/input").type("14/04/2023")
-          // cy.get("//tbody/tr[5]/td[2]/input").type("13/04/2023")
-        //   cy.get("//tbody/tr[6]/td[2]/span[1]/input").click()
-           cy.get("[value='Manual Tester']").click()           //proffession
+           //cy.get("//table/tbody/tr[5]/td[2]/input[@type='text']").type("16/4/2023",{force:true})
+           cy.get(":nth-child(5) > [width='65%'] > input").type("17 apr 2023",{force:true})
+           //cy.xpath('//table/tbody/tr[5]/td[2]//input[@type="text"]').type("10 April 2023",{force: true})
+           cy.get("[value='Manual Tester']").click()
+           cy.get("[type='file']").selectFile("/home/lenovo/swapnil/cypress/fixtures/Cypress.pdf",{force:true})
+           cy.get("[value='Selenium IDE']").check()
+        
+        
+        
+        
+           //   cy.get("//tbody/tr[6]/td[2]/span[1]/input").click()
+         //             //proffession
         //   cy.get("//tbody/tr[8]/td/span[2]/input[1] ").click()         // flovers of selenium 
           
 
