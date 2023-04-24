0bcffc99712114cf95690e5f6766cc921c595d4a
tpavaskar781 <tpavaskar781@gmail.com>
2023-04-24 12:43:03 +0530

    4.3 Drag and drop uodated

--- cypress/e2e/DragDrop.cy.js
+++ cypress/e2e/DragDrop.cy.js
@@ -2,7 +2,7 @@
         describe('Assignment 4.3',()=>{
             it('Drag and Drop',function(){
             cy.visit('https://demoqa.com/droppable')
-            cy.get('#draggable').drag('#simpleDropContainer > #droppable')
+            cy.get('#draggable').drag('#simpleDropContainer > #droppable',{force:true})
     
         })
 
